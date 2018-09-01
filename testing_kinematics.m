897f80f99db08b8679ca72904d29f09ca4912ae6
Lupasic <obulichev@yandex.ru>
2018-09-01 21:35:20 +0300

    IK for 3 links both hands are completed

--- testing_kinematics.m
+++ testing_kinematics.m
@@ -1,20 +1,20 @@
 clear;
 constants;
 
-ang = deg2rad([-105 150 135]');
+ang = deg2rad([-10 -100 -100]');
 
-[~, pos, ~] = FK3links(ang,"L",robot);
+[~, pos, ~] = FK3links(ang,"R",robot);
 
 fullMatOld = pos;
 
-newAngles = num_ik(fullMatOld,"L",robot);
+newAngles = IK3links(fullMatOld,"R",robot);
 
 rad2deg(newAngles)
 
 fullMatOld
 for curAngles=newAngles
 %     rad2deg(curAngles)
-    [~, pos, ~] = FK3links(curAngles,"L",robot);
+    [~, pos, ~] = FK3links(curAngles,"R",robot);
 fullMatNew = roundn(pos - fullMatOld,-3)
 % fullMatNew(3:6) = rad2deg(fullMatNew(3:6));
 % fullMatNew
